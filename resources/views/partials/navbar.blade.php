53f4cd5fa66f95b10c630b5fb9a8a8898593d287
Adam Jarvis <ajarvis@palantir.com>
2020-07-18 13:05:07 +0400

    Init Laravel 7.X

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,57 @@
+<nav class="navbar ">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+    </div>
+
+    <div id="navbar-menu">
+        <div id="navbar-start">
+            <div class="navbar-item has-dropdown is-hoverable">
+<div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link  is-active" href="/documentation/overview/start/">
+          Docs
+        </a>
+        <div class="navbar-dropdown ">
+          <a class="navbar-item " href="/documentation/overview/start/">
+            Overview
+          </a>
+          <a class="navbar-item " href="http://bulma.io/documentation/modifiers/syntax/">
+            Modifiers
+          </a>
+          <a class="navbar-item " href="http://bulma.io/documentation/columns/basics/">
+            Columns
+          </a>
+          <a class="navbar-item " href="http://bulma.io/documentation/layout/container/">
+            Layout
+          </a>
+          <a class="navbar-item " href="http://bulma.io/documentation/form/general/">
+            Form
+          </a>
+          <a class="navbar-item " href="http://bulma.io/documentation/elements/box/">
+            Elements
+          </a>
+
+          <a class="navbar-item is-active" href="http://bulma.io/documentation/components/breadcrumb/">
+              Components
+            </a>
+
+          <hr class="navbar-divider">
+          <div class="navbar-item">
+            <div>
+              <p class="is-size-6-desktop">
+                <strong class="has-text-info">0.5.1</strong>
+              </p>
+
+              <small>
+                  <a class="bd-view-all-versions" href="/versions">View all versions</a>
+                </small>
+
+            </div>
+          </div>
+        </div>
+      </div>
+            </div>
+        </div>
+    </div>
+</nav
